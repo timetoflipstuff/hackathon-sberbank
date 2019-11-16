8118c3e5c5a1b4c875970f86a671ce472f8f2ec3
Roman Kuzin <kuzin.r@mail.ru>
2019-11-16 12:47:31 +0300

    Folder tree

---
+++ hackaton-sberbank/hackaton-sberbank/Scenes/Manager/ManagerVC.swift
@@ -0,0 +1,9 @@
+//
+//  ManagerVC.swift
+//  hackaton-sberbank
+//
+//  Created by Roman Kuzin on 16.11.2019.
+//  Copyright © 2019 Sberbank-hackaton. All rights reserved.
+//
+
+import Foundation
---
+++ hackaton-sberbank/hackaton-sberbank/Scenes/Spendings/SpendingsVC.swift
@@ -0,0 +1,9 @@
+//
+//  SpendingsVC.swift
+//  hackaton-sberbank
+//
+//  Created by Roman Kuzin on 16.11.2019.
+//  Copyright © 2019 Sberbank-hackaton. All rights reserved.
+//
+
+import Foundation
